f2bf3bbaea97a35943af4a628d9e39b0e350fd01
Jovon Miller <millerjovon1@gmail.com>
2023-08-08 15:55:38 +0900

    finished

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit,:update, :destroy ]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      # give the form back again -> new.html.erb
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      # give the form back again -> edit.html.erb
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :detials, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
